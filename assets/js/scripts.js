012c6cd1f9133a2457c7584bd9c4a650642a1461
Akash Chondra Roy <97385572+Akash0467@users.noreply.github.com>
2023-06-28 01:35:10 +0600

    change
    change

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
-    $(".logo-icon-bottom").click(function(){
-        $(".menu").slideToggle(1000)
-    });
+    const toggleBtn = document.querySelector(".show");
+    const toggleBtnIcon = document.querySelector(".show i");
+    const dropDownMenu = document.querySelector(".menu");
+    toggleBtn.onclick = function(){
+        dropDownMenu.classList.toggle('open')
+        const isOpen = dropDownMenu.classList.contains('open')
+
+        toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars-staggered'
+    }
     $(".owl-carousel-5").owlCarousel({
         items:3,
         loop:true,
